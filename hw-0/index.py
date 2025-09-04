998272c1ff8376705fbf759e91c8dc1aee9e4a08
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-09-04 17:22:34 -0400

    FIX: Color assignment of points
    Remember that numpmy "arrays" are 2D, so even
    a single column converted to a ndarray will
    be a list of lists

--- hw-0/index.py
+++ hw-0/index.py
@@ -15,10 +15,11 @@ print(labels)
 # graph
 
 # get each attribute vector
-sepal_length = numpy.array(attributes)[:,0:1]
-sepal_width = numpy.array(attributes)[:,1:2]
-petal_length = numpy.array(attributes)[:,2:3]
-petal_width = numpy.array(attributes)[:,3:4]
+attributes_matrix = attributes.to_numpy()
+sepal_length = attributes_matrix[:,0:1]
+sepal_width = attributes_matrix[:,1:2]
+petal_length = attributes_matrix[:,2:3]
+petal_width = attributes_matrix[:,3:4]
 
 # create a graph using 2 attribute vectors (for non-same attributes) for all possible pairs
 # red is Iris-setosa, green is Iris-virginica, blue is Iris-versicolor
@@ -27,9 +28,11 @@ color_map = {
     'Iris-virginica': 'green',
     'Iris-versicolor': 'blue'
 }
-color_vector = [color_map[species] for species in labels]
+labels_array = labels.to_numpy().tolist()
+color_vector = [color_map[species[0]] for species in labels_array]
 attribute_arr = [sepal_length, sepal_width, petal_length, petal_width]
 attribute_category_strings = ["sepal length", "sepal width", "petal length", "petal width"]
+
 
 # loop over all possible combinations of attributes and create subplots
 fig, axes = plt.subplots(4,4, constrained_layout=True, figsize=(20,20))
