a81044b8fbab0e0b8264b7942fbc1bf24ed63985
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-09-09 18:47:42 -0400

    FEAT: Set up notebook

--- hw-1/warm_up.py
+++ hw-1/warm_up.py
@@ -3,9 +3,10 @@ import numpy as np
 import torch
 
 # 1. Let a = np.array([1,2,3,4,5,6,7,8]). Reshape a into a 2 by 4 matrix.
-a = np.array([1,2,3,4,5,6,7,8])
-a = np.array([a[0:4],a[4:8]])
-print(a.shape)
+def q1():
+    a = np.array([1,2,3,4,5,6,7,8])
+    a = np.array([a[0:4],a[4:8]])
+    print(a.shape)
 
 # 2. Let a be a pytorch tensor constructed with elements [1,3,5,6], and let b be a tensor constructed
 # with elements [5, 6, 8, 9]. Write a sequence of lines, one each to perform the following operations
@@ -17,19 +18,19 @@ print(a.shape)
 # • Dot product between a and b
 # • Dot product between an elementwise exponentiation of a with base e and an elementwise
 # natural logarithm of b
-a = torch.tensor([1,3,5,6])
-b = torch.tensor([5,6,8,9])
-addition = a+b
-multiplication = a*b 
-power = torch.pow(a,b)
-dot1 = torch.dot(a,b)
-dot2 = torch.dot(torch.exp(a), torch.log(b))
-
-print(addition)
-print(multiplication)
-print(power)
-print(dot1)
-print(dot2)
+def q2():
+    a = torch.tensor([1,3,5,6])
+    b = torch.tensor([5,6,8,9])
+    addition = a+b
+    multiplication = a*b 
+    power = torch.pow(a,b)
+    dot1 = torch.dot(a,b)
+    dot2 = torch.dot(torch.exp(a), torch.log(b))
+    print(addition)
+    print(multiplication)
+    print(power)
+    print(dot1)
+    print(dot2)
 
 # 3. Use tensor and autograd from the pytorch package to complete the following questions:
 # (a) Calculate the gradient of
@@ -41,12 +42,12 @@ print(dot2)
 
 # g is same as input vector ([x, y, z, k] * [x, y, z, k] * [e^x, e^y, e^z, e^k]) • [1, 3, 5, 6]
                             # can rewrite x^2 operation as diagonal matrix
-
-input = torch.tensor([5.,6.,8.,9.], requires_grad=True)
-g = torch.dot(torch.exp(input) * input * input, torch.tensor([1.,3.,5.,6.]))
-torch.autograd.backward(g)
-gradient = torch.grad()
-print(gradient)
+def q3_a():
+    input = torch.tensor([5.,6.,8.,9.], requires_grad=True)
+    g = torch.dot(torch.exp(input) * input * input, torch.tensor([1.,3.,5.,6.]))
+    torch.autograd.backward(g)
+    gradient = torch.grad()
+    print(gradient)
 
 # (b) Let A be a matrix with values [[4, 3], [7, 9]] and B be a matrix with values [[3, 5], [1, 11]]. Calcu-
 # late the gradient of the following function f(A) with respect to the entries of A evaluated at
@@ -56,14 +57,42 @@ print(gradient)
 # entries of the matrix inside the norm expression.
 # Hints: 1. to calculate matrix multiplication, you need to use the function torch.matmul; 2.
 # to calculate L2 norm, you need to use the function torch.norm() and set p = 2.
+def q3_b():
+    a = torch.tensor([[4,3],[7,9]])
+    b = torch.tensor([[3,5],[1,11]])
+    # matrix multiplication ABC = (AB)C
+    AB = torch.matmul(a,b)
+    AAt = torch.matmul(a, torch.transpose(a,0,1))
+    ABt = torch.matmul(a, torch.transpose(b,0,1))
+    At = torch.transpose(a,0,1)
+    inner_matrix_product = torch.matmul(At,torch.matmul(ABt, torch.matmul(AAt, AB)))
+    f = torch.log(torch.norm((inner_matrix_product), p=2))
+
 # (c) Calculate the gradient of
 # F ¡x, y¢ = tanh (x) + tanh ¡y¢
 # at the point ¡x = 3, y = 7¢.
+
+# tanhx = (expx - exp(-x))/(expx + exp(-x))
+
+
 # 4. Let a be a torch integer tensor containing the values [1, 2, 3].
 # 2
 # CS5785 Fall 2025: Homework 1 Page 3
 # • convert a to a numpy array and store it under a new variable b
 # • convert a into a float tensor
+def q4():
+    a = torch.tensor([1,2,3])
+    b = a.numpy()
+    a = torch.from_numpy(a).float()
+
 # 5. Answer the following questions using the package Numpy:
 # • What is the product of matrices of matrices [[1, 3, 5], [2, 1, 5]] and [[8, 4], [3, 6], [2, 7]]?
+def q5_a():
+    a = np.array([1,3,5],[2,1,5])
+    b = np.array([8,4],[3,6],[2,7])
+    print(np.matmul(a,b))
+
 # • What is the Frobenius norm of the 1 x 3 matrix [100, 2, 1]?
+def q5_b():
+    a = np.array([100,2,1])
+    print(np.norm(a))
